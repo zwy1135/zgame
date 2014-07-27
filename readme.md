14fafdc578430e9a5f0c7cf68767c83f7a5cf8c2
zwy1135 <zwy1135@gmail.com>
2014-07-27 17:32:31 +0800

    helloworld
    My first pygame programe

---
+++ readme.md
@@ -0,0 +1,5 @@
+
+# zgame
+==========================
+这是一个简单的pygame练习，参照的是目光博客的[教程](http://eyehere.net/2011/python-pygame-novice-professional-index/)
+
---
+++ src/helloworld.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 27 16:50:51 2014
+
+@author: wy
+"""
+
+background_img = "../data/image/sushiplate.jpg"
+mouse_img = "../data/image/fugu.png"
+
+import pygame
+from pygame.locals import *
+from sys import exit
+
+def main():
+    pygame.init()
+    
+    screen = pygame.display.set_mode((640,480),0,32)
+    
+    pygame.display.set_caption("hello world!")
+    
+    background = pygame.image.load(background_img).convert()
+    mouse = pygame.image.load(mouse_img).convert_alpha()
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+        
+        screen.blit(background,(0,0))
+        
+        x,y = pygame.mouse.get_pos()
+        
+        x -= mouse.get_width()/2
+        y -= mouse.get_height()/2
+        
+        screen.blit(mouse,(x,y))
+        
+        pygame.display.update()
+        
+if __name__ == "__main__":
+    main()
